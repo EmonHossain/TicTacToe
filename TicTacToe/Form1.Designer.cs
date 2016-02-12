58eef2d298cd511014b9dc5ab0087c5e542ed4b8
EmonHossain <emondipu@gmail.com>
2016-02-12 21:16:40 +0600

    The first commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Green;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(410, 12);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(94, 35);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play again";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click on squre box to play";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 248);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playButton);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+
+        private Button[] button;
+        private int x = 50, y = 50;
+        private int n = 1,checker=0;
+        private int flag = 1;
+        private int begin = 0;
+        private Label label;
+        private Timer timer,t;
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            button = new Button[9];
+            random = new Random(); 
+
+            timer = new Timer();
+            timer.Tick += timer_Tick;
+            timer.Interval = 800;
+
+            t = new Timer();
+            t.Interval = 500;
+            t.Tick += t_Tick;
+
+            for (int i = 0; i < 9; i++)
+            {
+                button[i] = new Button();
+                button[i].Size = new Size(50,50);
+                button[i].Location = new Point(x, y);
+                button[i].Click += Form1_Click;
+                Controls.Add(button[i]);
+                if (n == 3)
+                {
+                    y += 50;
+                    n = 0;
+                    x = 50;
+                }
+                else
+                    x+=50;
+                n++;
+            }
+            label = new Label();
+            label.Text = "Your turn";
+            label.Location = new Point(300, 120);
+            Controls.Add(label);
+
+            playButton.Enabled = false;
+            playButton.BackColor = Color.Red;
+
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            if (flag >= 5 && flag <=10)
+            {
+                if ((button[0].Text.Equals("X") && button[1].Text.Equals("X") && button[2].Text.Equals("X")) || (button[0].Text.Equals("X") && button[3].Text.Equals("X") && button[6].Text.Equals("X")) || (button[0].Text.Equals("X") && button[4].Text.Equals("X") && button[8].Text.Equals("X")) || (button[1].Text.Equals("X") && button[4].Text.Equals("X") && button[7].Text.Equals("X")) || (button[2].Text.Equals("X") && button[4].Text.Equals("X") && button[6].Text.Equals("X")) || (button[2].Text.Equals("X") && button[5].Text.Equals("X") && button[8].Text.Equals("X")) || (button[3].Text.Equals("X") && button[4].Text.Equals("X") && button[5].Text.Equals("X")) || (button[6].Text.Equals("X") && button[7].Text.Equals("X") && button[8].Text.Equals("X")))
+                {
+                    t.Enabled = false;
+                    t.Stop();
+                    timer.Enabled = false;
+                    timer.Stop();
+                    deactivator();
+                    label.Text = "";
+                    playButton.Enabled = true;
+                    MessageBox.Show("You won....!!!");
+                    checker = 0;
+                }
+                else if ((button[0].Text.Equals("O") && button[1].Text.Equals("O") && button[2].Text.Equals("O")) || (button[0].Text.Equals("O") && button[3].Text.Equals("O") && button[6].Text.Equals("O")) || (button[0].Text.Equals("O") && button[4].Text.Equals("O") && button[8].Text.Equals("O")) || (button[1].Text.Equals("O") && button[4].Text.Equals("O") && button[7].Text.Equals("O")) || (button[2].Text.Equals("O") && button[4].Text.Equals("O") && button[6].Text.Equals("O")) || (button[2].Text.Equals("O") && button[5].Text.Equals("O") && button[8].Text.Equals("O")) || (button[3].Text.Equals("O") && button[4].Text.Equals("O") && button[5].Text.Equals("O")) || (button[6].Text.Equals("O") && button[7].Text.Equals("O") && button[8].Text.Equals("O")))
+                {
+                    t.Enabled = false;
+                    t.Stop();
+                    timer.Enabled = false;
+                    timer.Stop();
+                    deactivator();
+                    label.Text = "";
+                    playButton.Enabled = true;
+                    MessageBox.Show("You lose..!!!");
+                    checker = 0;
+                }
+                else if (flag == 10 && checker != 0)
+                {
+                    t.Enabled = false;
+                    t.Stop();
+                    timer.Enabled = false;
+                    timer.Stop();
+                    deactivator();
+                    label.Text = "";
+                    MessageBox.Show("Tie");
+                }
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (flag % 2 == 0)
+            {
+                while (flag%2==0) { 
+                    int rand = random.Next(0, 8);
+                    if (!button[rand].Text.Equals("O") && !button[rand].Text.Equals("X"))
+                    {
+                        button[rand].Text = "O";
+                        label.Text = "Your turn";
+                        flag++;
+                        if(flag>=9){
+                            timer.Enabled = false;
+                            timer.Stop();
+                        }
+                    }
+                }
+            }
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            if (begin == 0)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (sender.Equals(button[i]))
+                    {
+                        button[i].Text = "X";
+                        //button[i].Enabled = false;
+                        label.Text = "Computer turn";
+                        flag++;
+                    }
+                }
+                timer.Enabled = true;
+                timer.Start();
+
+                
+                t.Enabled = true;
+                t.Start();
+
+                begin++;
+            }
+            if (sender.Equals(button[0]) && !button[0].Text.Equals("O") && !button[0].Text.Equals("X"))
+            {
+                button[0].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[1]) && !button[1].Text.Equals("O") && !button[1].Text.Equals("X"))
+            {
+                button[1].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[2]) && !button[2].Text.Equals("O") && !button[2].Text.Equals("X"))
+            {
+                button[2].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[3]) && !button[3].Text.Equals("O") && !button[3].Text.Equals("X"))
+            {
+                button[3].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[4]) && !button[4].Text.Equals("O") && !button[4].Text.Equals("X"))
+            {
+                button[4].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[5]) && !button[5].Text.Equals("O") && !button[5].Text.Equals("X"))
+            {
+                button[5].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[6]) && !button[6].Text.Equals("O") && !button[6].Text.Equals("X"))
+            {
+                button[6].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[7]) && !button[7].Text.Equals("O") && !button[7].Text.Equals("X"))
+            {
+                button[7].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }
+            else if (sender.Equals(button[8]) && !button[8].Text.Equals("O") && !button[8].Text.Equals("X"))
+            {
+                button[8].Text = "X";
+                label.Text = "Computer turn";
+                flag++;
+            }           
+        }
+        private void deactivator()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                button[i].Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i < 9; i++)
+            {
+                button[i].Text = null;
+                button[i].Enabled = true;
+            }
+            flag = 1;
+            begin = 0;
+            playButton.Enabled = false;
+            playButton.BackColor = Color.Red;
+        }
+    }
+}
